5e1671dc55134a1951130e8f427d0845be33d009
gzq <942560379@qq.com>
2017-07-28 23:18:35 +0800

    fff

--- src/main/java/ing/gzq/MrcourseApplication.java
+++ src/main/java/ing/gzq/MrcourseApplication.java
@@ -3,12 +3,19 @@ package ing.gzq;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("ing.gzq.dao")
-public class MrcourseApplication {
+public class MrcourseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MrcourseApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MrcourseApplication.class);
 	}
 }
