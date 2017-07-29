8c6cd0a492966b88fbc383eb75c8f2d8eb84d642
depressedX <yuri8023@qq.com>
2017-07-29 15:00:06 +0800

    阶段

--- src/main/resources/static/script/teacher-main.js
+++ src/main/resources/static/script/teacher-main.js
@@ -12,17 +12,57 @@ require(['userHandler'],function (userHandlerModule) {
     }
 
     teacherClient.login(info);
+
+    window.console.log = function () {
+        var text = '';
+        for (var i in arguments){
+            text+=" " + arguments[i];
+        }
+        var tmp = document.createElement("DIV");
+        tmp.appendChild(document.createTextNode(text));
+        document.body.appendChild(tmp)
+    }
+
+    /****************************************************************/
+    /*                       创建课程                                */
+    /****************************************************************/
+
     // teacherClient.createCourse({
     //     name:"高数",
     //     introduce:"hahaha高数啊"
     // })
-    teacherClient.getCourse();
+    // teacherClient.on("createcoursesuccess",function (data) {
+    //     console.log(typeof data,data)
+    // })
 
-    teacherClient.on("createcoursesuccess",function (data) {
-        console.log(typeof data,data)
-    })
+
+    /****************************************************************/
+    /*                       获取课程                                */
+    /****************************************************************/
+    teacherClient.getCourse();
     teacherClient.on("getcoursesuccess",function (data) {
         console.log(typeof data,data)
     })
+
+
+    /****************************************************************/
+    /*                       公告部分                          */
+    /****************************************************************/
+
+    teacherClient.uploadNotice(1,{
+        content:"first notice"
+    });
+    teacherClient.on("uploadnoticesuccess",function () {
+        console.log("uploadnoticesuccess")
+    });
+
+
+
+
+
+
+
+
+
 
 })
