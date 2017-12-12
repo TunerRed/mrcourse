9de2e098eab777323979db4f4f080add522a1deb
TunerRed <codevoid001@gmail.com>
2017-12-12 16:02:05 +0800

    Update Result.java

--- src/main/java/ing/gzq/base/Result.java
+++ src/main/java/ing/gzq/base/Result.java
@@ -3,9 +3,6 @@ package ing.gzq.base;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Created by 王镜鑫 on 17-4-23 下午10:04.
- */
 public class Result {
     public Result(int status,String message){
         this.status = status;
